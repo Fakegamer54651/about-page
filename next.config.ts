3d30b095a1519728cdaa3fdeef75f8217f1f6f81
abek01sulaymonov <abek01sulaymonov@gmail.com>
2025-12-04 18:43:47 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
   trailingSlash: true,
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
